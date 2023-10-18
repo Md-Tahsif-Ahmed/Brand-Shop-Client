9aaf67ed276e342b36506c2f107f8beeff135dba
tahsif <tahsif.cse@gmail.com>
2023-10-18 14:48:05 +0600

    Added Error Page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../components/Home/Home";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
   
 const router = createBrowserRouter([
     {
       path: "/",
       element:  <App></App>,
+      errorElement: <ErrorPage></ErrorPage>,
       children: [
         {
             path: '/',
