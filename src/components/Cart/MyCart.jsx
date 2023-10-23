9ed1c2032caafb26acb0fdb8710973117d13dc4e
tahsif <tahsif.cse@gmail.com>
2023-10-23 11:46:45 +0600

    Add Mycart work and delete

--- src/components/Cart/MyCart.jsx
+++ src/components/Cart/MyCart.jsx
@@ -1,8 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
-import { Link } from "react-router-dom";
 import Swal from 'sweetalert2';
- 
 
 const MyCart = () => {
   const { user } = useContext(AuthContext);
@@ -15,31 +13,47 @@ const MyCart = () => {
       .catch((error) => console.error(error));
   }, []);
 
-  const handleDelete = _id =>{ 
-    fetch(`http://localhost:7000/mycart/${_id}`,{
-        method: 'DELETE',
-    })
-    .then(res => res.json())
-    .then(data=>{
-        if(data.deleteCount > 0){
-            Swal.fire({
-                title: 'Success!',
-                text: 'Product Successfully Deleted',
-                icon: 'success',
-                confirmButtonText: 'done'
-              })
-        }
-    })
+  const handleDelete = (_id) => {
+    console.log(_id)
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:7000/mycart/${_id}`, {
+          method: 'DELETE'
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log('bala',data)
+            if (data.deletedCount > 0) {
+              Swal.fire('Deleted!', 'Your product has been deleted.', 'success');
+              // Update the UI by filtering out the deleted product
+            //   const remainingProducts = cartData.filter((d) => d._id !== id);
+            //   setCartData(remainingProducts);
+            } else {
+              Swal.fire('Error!', 'Failed to delete the product.', 'error');
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+            Swal.fire('Error!', 'An error occurred while deleting the product.', 'error');
+          });
+      }
+    });
   }
 
   return (
     <div>
       <div className="overflow-x-auto">
         <table className="table">
-          {/* head */}
           <thead>
             <tr>
-           
               <th>Product Name</th>
               <th>Brand</th>
               <th>Type</th>
@@ -48,16 +62,14 @@ const MyCart = () => {
             </tr>
           </thead>
           <tbody>
-            {/* rows */}
-            {cartData.map((data, ) => (
-              <tr key={data._id}> {/* Use a unique key for each row */}
-               
+            {cartData.map((data) => (
+              <tr key={data._id}>
                 <td>{data.name}</td>
                 <td>{data.brand}</td>
                 <td>{data.type}</td>
                 <td>{data.price}</td>
                 <td>
-                  <Link to='/delete'><button onClick={()=>handleDelete(data._id)} className="btn bg-red-600 rounded-lg ">Delete</button></Link>
+                  <button onClick={() => handleDelete(data._id)} className="btn bg-red-600 rounded-lg">X</button>
                 </td>
               </tr>
             ))}
